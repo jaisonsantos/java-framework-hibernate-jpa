1f2ecc9526cd708fe7608eab3379f2180487c894
Bruno Penha <cm.java.frameworks@gmail.com>
2015-05-19 10:22:18 -0300

    Foram incluídos exemplos de consultas usando a API Criteria.

--- drop.sql
+++ drop.sql
@@ -10,8 +10,6 @@ ALTER TABLE t_livro_heranca_juntada DROP CONSTRAINT tlvrhrancajuntadaD
 ALTER TABLE T_livro_heranca_abstrata DROP CONSTRAINT TlvrhrncaabstrataD
 ALTER TABLE t_consumidor_manip DROP CONSTRAINT tcnsmdrmanipcndrco
 ALTER TABLE t_consumidor_consulta DROP CONSTRAINT tcnsmdrcnsltacndrc
-ALTER TABLE t_recibo_t_item_compra DROP CONSTRAINT trcbttmcmptnsCmprD
-ALTER TABLE t_recibo_t_item_compra DROP CONSTRAINT trcbttemcompraRcbD
 ALTER TABLE t_junc_arts_cd DROP CONSTRAINT tjunc_arts_cdce_cd
 ALTER TABLE t_junc_arts_cd DROP CONSTRAINT tjncartscdcartista
 ALTER TABLE t_noticia_ordenado_t_comentarios_ordenado DROP CONSTRAINT tntcrdndtcmcmntrsD
@@ -20,6 +18,8 @@ ALTER TABLE t_noticia_indexado_t_comentario_indexado DROP CONSTRAINT tntcndxdtcN
 ALTER TABLE t_noticia_indexado_t_comentario_indexado DROP CONSTRAINT tntcndxdtcmcmntrsD
 ALTER TABLE t_etiquetas DROP CONSTRAINT ttqtsLvrClcoListID
 ALTER TABLE t_trilha DROP CONSTRAINT ttrlhaCDClcaoMapID
+ALTER TABLE t_recibo_t_item_compra DROP CONSTRAINT trcbttmcmptnsCmprD
+ALTER TABLE t_recibo_t_item_compra DROP CONSTRAINT trcbttemcompraRcbD
 DROP TABLE LIVRO
 DROP TABLE PESSOA
 DROP TABLE t_endereco
@@ -35,7 +35,6 @@ DROP TABLE t_consumidor_com_endereco
 DROP TABLE t_consumidor_11uni
 DROP TABLE t_consumidor_11uni_join
 DROP TABLE t_item_compra
-DROP TABLE t_recibo
 DROP TABLE t_item_compra_join_column
 DROP TABLE t_recibo_join_column
 DROP TABLE t_item_compra_join_column_2
@@ -69,10 +68,11 @@ DROP TABLE t_endereco_manip
 DROP TABLE t_consumidor_consulta
 DROP TABLE t_endereco_consulta
 DROP TABLE t_livro_consulta
-DROP TABLE t_recibo_t_item_compra
+DROP TABLE t_recibo
 DROP TABLE t_junc_arts_cd
 DROP TABLE t_noticia_ordenado_t_comentarios_ordenado
 DROP TABLE t_noticia_indexado_t_comentario_indexado
 DROP TABLE t_etiquetas
 DROP TABLE t_trilha
+DROP TABLE t_recibo_t_item_compra
 DELETE FROM SEQUENCE WHERE SEQ_NAME = 'SEQ_GEN'
